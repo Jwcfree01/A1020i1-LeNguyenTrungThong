6386a4dc8e2c4e00c01aa03e7853a8d349afca5f
Jwcfree01 <letrungthong14042000@gmail.com>
2021-01-22 16:06:25 +0700

    B5

--- A1020i1-LeNguyenTrungThong/Modul2/src/B5_Access_Modifier/BaiTap/AccessModifier.java
+++ A1020i1-LeNguyenTrungThong/Modul2/src/B5_Access_Modifier/BaiTap/AccessModifier.java
@@ -1,4 +1,31 @@
 package B5_Access_Modifier.BaiTap;
 
 public class AccessModifier {
+    private double radius = 1.0;
+    private String color = "red";
+
+    void Circle(){
+
+    }
+
+    void circle(double r){
+
+    }
+
+    public double getRadius(){
+        return radius;
+    }
+
+    public double getArea(){
+        return radius*radius*Math.PI;
+    }
+
+    public static void main(String[] args) {
+        AccessModifier circle1 = new AccessModifier();
+        circle1.getArea();
+        circle1.getRadius();
+
+        circle1.radius =10;
+        System.out.println(circle1.getArea());
+    }
 }
--- A1020i1-LeNguyenTrungThong/Modul2/src/B5_Access_Modifier/ThucHanh/StaticMethod.java
+++ A1020i1-LeNguyenTrungThong/Modul2/src/B5_Access_Modifier/ThucHanh/StaticMethod.java
@@ -4,7 +4,7 @@ public class StaticMethod {
 
     private int rollno;
     private String name;
-    private static String college = "BBDIT";
+    private static String college = "Codegym";
 
     //constructor to initialize the variable
     StaticMethod(int r, String n) {
@@ -13,15 +13,25 @@ public class StaticMethod {
     }
 
     //static method to change the value of static variable
-    static void change() {
-        college = "CODEGYM";
-    }
+//    static void change() {
+//        college = "CODEGYM";
+//    }
 
     //method to display values
     void display() {
         System.out.println(rollno + " " + name + " " + college);
     }
     public static void main(String[] args) {
+//        StaticMethod.change(); //calling change method
 
+        //creating objects
+        StaticMethod s1 = new StaticMethod(111, "Hoang");
+        StaticMethod s2 = new StaticMethod(222, "Khanh");
+        StaticMethod s3 = new StaticMethod(333, "Nam");
+
+        //calling display method
+        s1.display();
+        s2.display();
+        s3.display();
     }
 }
