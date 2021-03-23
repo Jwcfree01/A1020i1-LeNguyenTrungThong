12d7988dd99a79b44f5730a9f8e18db937df4c48
Jwcfree01 <letrungthong14042000@gmail.com>
2021-03-23 22:09:59 +0700

    aa

---
+++ A1020i1-LeNguyenTrungThong/Modul2/src/CaseStudy/Common/Exception/Exception.java
@@ -0,0 +1,12 @@
+package CaseStudy.Common;
+
+public class Exception extends java.lang.Exception {
+    public Exception(){
+        super();
+    }
+
+    public Exception(String str){
+        super(str);
+    }
+
+}
---
+++ A1020i1-LeNguyenTrungThong/Modul2/src/CaseStudy/Common/Exception/NameException.java
@@ -0,0 +1,7 @@
+package CaseStudy.Common;
+
+public class Check_Exception extends Exception{
+    public Check_Exception(){
+        super("Ten khong hop le");
+    }
+}
