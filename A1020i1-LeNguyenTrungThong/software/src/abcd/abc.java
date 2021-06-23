847c9767820a5971a9a83e86bd02dd0e81047003
Jwcfree01 <61874406+Jwcfree01@users.noreply.github.com>
2021-06-23 07:57:16 +0700

    soft

---
+++ A1020i1-LeNguyenTrungThong/software/src/abcd/abc.java
@@ -0,0 +1,10 @@
+package abcd;
+
+public class abc {
+    public static int main(String[] args) {
+        int a =5, *b, c;
+        b=&a;
+        System.out.println("%d", a* *b * a + *b);
+        return (0);
+    }
+}
